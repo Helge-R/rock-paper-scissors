285d085e8fccdcd001e6eac0d2ffb2e2f9dff5d3
Helge-R <109223097+Helge-R@users.noreply.github.com>
2023-03-31 15:02:22 +0200

    Finish the basic structure of the game

--- script.js
+++ script.js
@@ -61,8 +61,7 @@ let gameFinished = false;
 for (btn of Array.from(buttons)) {
   btn.addEventListener("click", (e) => {
     if (gameFinished) return;
-    let playerSelection = e.target.parentElement.classList[0][0].toUpperCase() + e.target.parentElement.classList[0].slice(1);
-    console.log(playerSelection);
+    let playerSelection = e.currentTarget.classList[0][0].toUpperCase() + e.currentTarget.classList[0].slice(1);
     let computerSelection = getComputerChoice();
     let winner = playRound(playerSelection, computerSelection);
     winner = winner[0].toUpperCase() + winner.slice(1);
@@ -74,12 +73,16 @@ for (btn of Array.from(buttons)) {
     computerList.appendChild(computerChoice).textContent = computerSelection;
 
     
+    
 
     if (winner === "Player") {
       playerWins++;
     } else if (winner === "Computer") {
       computerWins++;
     }
+    
+    playerList.firstElementChild.textContent = "Player(" + playerWins + ")";
+    computerList.firstElementChild.textContent = "Computer(" + computerWins + ")"; 
 
     if (playerWins === 5 || computerWins === 5) {
       gameFinished = true;
@@ -101,11 +104,13 @@ restartBtn.addEventListener('click', (e) => {
   computerWins = 0;
   gameFinished = false;
   gameResult.textContent = "";
+  playerList.firstElementChild.textContent = "Player(" + playerWins + ")";
+    computerList.firstElementChild.textContent = "Computer(" + computerWins + ")"; 
   while (playerList.childElementCount > 1) {
-    playerList.removeChild(playerList.lastChild);
+    playerList.removeChild(playerList.lastElementChild);
   }
 
   while (computerList.childElementCount > 1) {
-    computerList.removeChild(computerList.lastChild);
+    computerList.removeChild(computerList.lastElementChild);
   }
 });
